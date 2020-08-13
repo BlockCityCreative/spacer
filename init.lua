78af9c37d9448b9df82fd33263782d00c4abd70e
Sr <samarthr1@outlook.com>
2020-08-13 16:20:37 +0530

    yet another try

--- init.lua
+++ init.lua
@@ -5,9 +5,10 @@ minetest.register_chatcommand("space", {
     description = "adds a node under a player, if they are 1) In space \n2) NOT in a protected area \n3)have not used it in the last 15 seconds",
     func = function(name)
         local player = minetest.get_player_by_name(name)
-        for k in pairs(player) do
-            print(player[k])
-        end   
+        local pos = player:get_pos()
+        for k in pairs(pos) do
+            print(pos[k])
+        end
        
     end
 })
