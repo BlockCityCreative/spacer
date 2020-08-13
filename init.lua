a852aa4d2212dfff72f913843508ec69c4836088
Sr <samarthr1@outlook.com>
2020-08-13 15:43:25 +0530

    registered chat command

---
+++ init.lua
@@ -0,0 +1,9 @@
+local storage = minetest.get_mod_storage()
+print("[spacer] loaded")
+
+minetest.register_chatcommand("space", {
+    description = "adds a node under a player, if they are 1) In space \n2) NOT in a protected area \n3)have not used it in the last 15 seconds",
+    func = function(name)
+       minetest.chat_send_all(minetest.get_player_by_name(name))
+    end
+})
