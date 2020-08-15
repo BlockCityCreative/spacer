97c8748de1d98339afe1ac0e55a50239100f0572
Samarth Ramesh <srinivas@Tirumala.home>
2020-08-16 00:47:14 +0530

    commited em all. added a command to place a node

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
-local storage = minetest.get_mod_storage()
 local maxTimes = 6-- maximum times -1 the command space can be used on one loading of the mod
+local spaceBegin = 20000
+
 print("[spacer] loaded")
 local numTimes= {}
 minetest.register_chatcommand("space", {
@@ -7,19 +8,27 @@ minetest.register_chatcommand("space", {
     func = function(name)
         local player = minetest.get_player_by_name(name)
         local pos = player:get_pos()
-        local area= areas:getAreasAtPos(pos)
-        local numAreas= 0
-        
-        for k in pairs(area) do 
-            numAreas=numAreas+1 -- gets no of areas at pos
-        end
+        local canPlace = areas:canInteract(pos,name)
 
-        if (pos["y"]>=20000 and numAreas==0 and ((not numTimes[name]) or numTimes[name] >= maxTimes) ) then
+        if (pos["y"]>= spaceBegin and canPlace and ((not numTimes[name]) or numTimes[name] < maxTimes) ) then
             minetest.set_node(pos, {name="default:dirt"})
             numTimes[name]=(numTimes[name]or 0) +1
             print(numTimes[name])
         else
-            print("hey you idiot get out of a protected place/goto space")
+            if pos["y"] < spaceBegin then
+                minetest.chat_send_player(name, "goto space please. in this server, space begins at"..spaceBegin)
+            end
+
+            if not canPlace then
+                minetest.chat_send_player(name, "You have been suspected of griefing. this incident will be reported")
+                print(name.."attempts to place node at protected area at pos"..tostring(pos["x"])..", "..tostring(pos["y"])..", "..tostring(pos["z"]))
+                
+            end
+
+            if numTimes >= maxTimes then
+                minetest.chat_send_player(name, "you are ssuspected of spamming the comman to place a block.please wait until a reboot before retrying. this incident will be reported")
+                print(name.."used the space command multiple times. the command was again executed at "..tostring(pos["x"])..", "..tostring(pos["y"])..", "..tostring(pos["z"]))
+            end
         end
     end
 })
