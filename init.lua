d760ec820162919507b7495bf97dd94af13238cf
Samarth Ramesh <srinivas@Tirumala.home>
2020-08-16 00:48:46 +0530

    fixed a typo

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ local spaceBegin = 20000
 print("[spacer] loaded")
 local numTimes= {}
 minetest.register_chatcommand("space", {
-    description = "adds a node under a player, if they are 1) In space \n2) NOT in a protected area \n3)have not used it in the last 15 seconds",
+    description = "adds a node under a player, if they are \n1) In space \n2) NOT in a protected area \n3)have not used it in the last 15 seconds",
     func = function(name)
         local player = minetest.get_player_by_name(name)
         local pos = player:get_pos()
