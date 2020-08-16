180307c5269f890ae2b1706abc3803d855735024
Samarth Ramesh <srinivas@seshachalam.localdomain>
2020-08-16 13:32:32 +0530

    removed dep on default

--- init.lua
+++ init.lua
@@ -1,7 +1,20 @@
 local maxTimes = 6-- maximum times -1 the command space can be used on one loading of the mod
-local spaceBegin = 20000
+local spaceBegin = 20002
 
-print("[spacer] loaded")
+if not minetest.get_modpath("default") then
+    minetest.register_node("spacer:seed", {
+        description = "seed for creating a new base in space, initialized and used only if default is not installed",
+        tiles = {"seed.png"},
+        groups = {oddly_breakable_by_hand=3,not_in_creative_inventory=1},
+        drop = ""
+    })
+    local isDefault = false
+
+else
+    isDefault = true
+
+end
+
 local numTimes= {}
 minetest.register_chatcommand("space", {
     description = "adds a node under a player, if they are \n1) In space \n2) NOT in a protected area \n3)have not used it in the last 15 seconds",
@@ -9,9 +22,16 @@ minetest.register_chatcommand("space", {
         local player = minetest.get_player_by_name(name)
         local pos = player:get_pos()
         local canPlace = areas:canInteract(pos,name)
-
+        pos["y"] = pos["y"] -2
         if (pos["y"]>= spaceBegin and canPlace and ((not numTimes[name]) or numTimes[name] < maxTimes) ) then
-            minetest.set_node(pos, {name="default:dirt"})
+            
+            if isDefault then
+                minetest.set_node(pos, {name="default:dirt"})
+            
+            else
+                minetest.set_node(pos, {name="spacer:seed"})
+            end
+            
             numTimes[name]=(numTimes[name]or 0) +1
             print(numTimes[name])
         else
@@ -25,7 +45,7 @@ minetest.register_chatcommand("space", {
                 
             end
 
-            if numTimes >= maxTimes then
+            if (  numTimes[name] or 0) >= maxTimes then
                 minetest.chat_send_player(name, "you are ssuspected of spamming the comman to place a block.please wait until a reboot before retrying. this incident will be reported")
                 print(name.."used the space command multiple times. the command was again executed at "..tostring(pos["x"])..", "..tostring(pos["y"])..", "..tostring(pos["z"]))
             end
